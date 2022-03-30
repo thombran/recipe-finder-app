823b930c22fa3edc0c6c404950748425c9b20e82
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2022-03-29 21:10:30 -0400

    add basic login component & add background

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <AppHeader/>
     <LoginComponent/>
-    <AppFooter/>
   </div>
 </template>
 
@@ -29,6 +27,7 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('./assets/recipe-background.jpg');
+  background-size: 100%;
 }
 </style>
--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,17 +1,102 @@
 <template>
   <div id="login">
-    
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="w-25 border border-dark rounded bg-light">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        class="text-dark"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" class="text-dark" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="success" id="submitButton">Submit</b-button>
+      <b-button type="reset" variant="danger" id="resetButton">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
+import {
+  BForm,
+  BCard,
+  BFormGroup,
+  BButton,
+  BFormInput,
+} from "bootstrap-vue";
 
-@Component
+@Component({
+  components: {
+    BForm,
+    BCard,
+    BFormGroup,
+    BButton,
+    BFormInput
+  }
+})
 export default class LoginComponent extends Vue {
-  
+  form = {
+    email: "",
+    name: "",
+  };
+  show = true;
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    alert(JSON.stringify(this.form));
+  }
+
+  onReset(event: Event) {
+    event.preventDefault();
+    // Reset our form values
+    this.form.email = "";
+    this.form.name = "";
+    // Trick to reset/clear native browser form validation state
+    this.show = false;
+    this.$nextTick(() => {
+      this.show = true;
+    });
+  }
 }
 </script>
 
 <style scoped>
+#login {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+  margin-left: 10%;
+}
+#input-group-1 {
+  margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+#input-group-2 {
+  margin-bottom: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+#resetButton, #submitButton {
+  margin-bottom: 5%;
+}
 </style>
