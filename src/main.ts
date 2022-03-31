a545ce191691c30327359ffd86c5ef34abcb8e3b
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2022-03-31 17:30:31 -0400

    add firebase config file and refs

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 /* eslint-disable vue/multi-word-component-names */
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import  firebaseConfig  from "./myConfig";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount('#app')
+}).$mount('#app');
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
