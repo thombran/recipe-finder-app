2e8926e40c3dc79430491a275b086aeb91483466
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2022-04-01 10:30:09 -0400

    modify nav icons and links, condense into component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="ma-12 pa-12 nav">
+      <v-navigation-drawer absolute permanent left expand-on-hover dark>
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img
+                src="https://randomuser.me/api/portraits/women/85.jpg"
+              ></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Sandra Adams
+              </v-list-item-title>
+              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Account</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-book-open-page-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Saved Recipes</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-checkbox-marked-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Completed Recipes</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-message-draw</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Reviews</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-exclamation-thick</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Popular</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-food</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Meal Prep</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Search</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+
+@Component
+export default class NavBar extends Vue {
+    
+}
+</script>
